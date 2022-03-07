3dc1f7c50da4a1585a38dcc7c6845ecabcc6bcad
Mostafijur Rahman <mrkajol550@gmail.com>
2022-03-07 15:37:38 +0600

    sketch readiness

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+    arrays_checking: ['Title','Say your Name', 'Body','Not a real message'],
+    methods: {  },
+    func: () => {
+        console.log('Hello World');
+        return "Hello From Func";
+    }
+}
+})
